d09af0ed6145d37cf60cd82a4a56a2a50425994b
Vivian Davila <viviandavilacodes@gmail.com>
2025-06-02 14:41:36 -0700

    Add starter code for decisions code along

---
+++ decisions_code_along/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Decisions App!");
+        Scanner console = new Scanner(System.in);
+
+        //snooze the alarm when you are tired (if-else)
+        System.out.println("Wake Up");
+
+        //decide to stop at the stop light (else-if)
+        System.out.println("Approaching stop light!");
+
+        //decide to speed through a yellow stop light (nested-if)
+        System.out.println("Approaching stop light changing color!");
+
+        //decide which direction your adventure will go in? (switch)
+        System.out.println("Which way do you want to go?");
+
+
+
+
+    }
+}
