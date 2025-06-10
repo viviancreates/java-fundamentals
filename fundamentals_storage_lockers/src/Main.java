c300c0effe9b9499b2d3bbe79ae48d40e7d58e35
Vivian Davila <viviandavilacodes@gmail.com>
2025-06-10 10:28:06 -0700

    Lockers and state being saved to array it seems

--- fundamentals_storage_lockers/src/Main.java
+++ fundamentals_storage_lockers/src/Main.java
@@ -5,31 +5,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
+        //moved outside loop bc do not want to create new lockers each time
+        LockerService lockerService = new LockerService(5);
+
+
         //create an instance method
         //doing this allows us to not call scanner eaxh time
         UserIO io = new UserIO();
 
-        //attach method to the class to print the menu
-        io.printMenu();
+        while(true) {
+            //attach method to the class to print the menu
+            io.printMenu();
 
 
+            //prompt for user choice
+            System.out.println("Choose Menu option: ");
+            int choice = io.getIntInput("Enter the option: ");
 
-        //prompt for user choice
-        System.out.println("Choose Menu option: ");
-        int choice = io.getIntInput("Enter the option: ");
+            lockerService.printLockerStatus();
 
-        LockerService lockerService = new LockerService(5);
-        lockerService.printLockerStatus();
+            if (choice == 1) {
+                lockerService.rentLocker();
+            } else if (choice == 2) {
+                lockerService.accessLocker();
+            } else if (choice == 3) {
+                lockerService.releaseLocker();
+            } else {
+                System.out.println("Invalid choice. Pick again pls.");
+                break;
+            }
 
-        //reminder to break the loop
-        if (choice == 1) {
-            lockerService.rentLocker();
-        }else if(choice == 2){
-            lockerService.accessLocker();
-        }else if(choice == 3){
-            lockerService.releaseLocker();
-        }else{
-            System.out.println("Invalid choice. Pick again pls.");
         }
     }
 
