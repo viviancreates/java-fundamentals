6a0086609def21dacbf36d337132ad867fbaef59
Vivian Davila <viviandavilacodes@gmail.com>
2025-06-11 09:19:30 -0700

    Practice classes and objects, save and pause

---
+++ objects_and_classes_practice/fizzbuzz/src/UserIO.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class UserIO {
+    Scanner console = new Scanner(System.in);
+
+    public void print(String message) {
+        System.out.println(message);
+    }
+}
