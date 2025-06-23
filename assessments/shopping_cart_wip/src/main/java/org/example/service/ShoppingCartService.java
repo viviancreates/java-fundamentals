a12246658b611055ba05f25cc566366dd9854e24
Vivian Davila <viviandavilacodes@gmail.com>
2025-06-23 14:24:13 -0700

    Add methods to the shopping cart service

--- assessments/shopping_cart_wip/src/main/java/org/example/service/ShoppingCartService.java
+++ assessments/shopping_cart_wip/src/main/java/org/example/service/ShoppingCartService.java
@@ -1,4 +1,39 @@
 package org.example.service;
 
+import model.Item;
+import java.util.ArrayList;
+
 public class ShoppingCartService {
+    private ArrayList<Item> items;
+
+    //creates new empty list
+    public ShoppingCartService() {
+        items = new ArrayList<>();
+    }
+
+    //add item method with price
+    public void addItem(String name, double price) {
+        items.add(new Item(name, price));
+    }
+
+    //remove item method
+    public Item removeItem(int index) {
+        return items.remove(index);
+    }
+
+    //return items in the shopping cart
+    public ArrayList<Item> getItems() {
+        //not returning actual list, creates copy
+        return new ArrayList<>(items);
+    }
+
+    public double getTotal() {
+        doublt total = 0;
+        for (Item item : items) {
+            total += item.price;
+        }
+        return total;
+    }
+
+
 }
