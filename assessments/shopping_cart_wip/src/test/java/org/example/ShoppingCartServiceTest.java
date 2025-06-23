5d505e672772aa113640ab80f69b9abc8c114efb
Vivian Davila <viviandavilacodes@gmail.com>
2025-06-23 15:16:54 -0700

    Pause

--- assessments/shopping_cart_wip/src/test/java/org/example/ShoppingCartServiceTest.java
+++ assessments/shopping_cart_wip/src/test/java/org/example/ShoppingCartServiceTest.java
@@ -4,6 +4,7 @@ import org.example.service.ShoppingCartService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ShoppingCartServiceTest {
@@ -13,5 +14,17 @@ public class ShoppingCartServiceTest {
         Assertions.assertTrue(sc.getItems().isEmpty());
     }
 
+    @Test
+    public void canAddItem() {
+        //create the SC service
+        ShoppingCartService sc = new ShoppingCartService();
+        //create a party
+        Item i = new Item("Shirt", 50.0 );
+        sc.addItem(i);
+
+        assertEquals(50.0, sc.getItems().size());
+        //assertEquals(p, wl.getList().get(0));
+
+    }
 
 }
