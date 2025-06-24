bc81f57418c9a8ae08a725e7e92518f00d4bb43c
Vivian Davila <viviandavilacodes@gmail.com>
2025-06-24 12:02:25 -0700

    Save

--- assessments/shopping_cart_wip/src/main/java/org/example/App.java
+++ assessments/shopping_cart_wip/src/main/java/org/example/App.java
@@ -1,10 +1,50 @@
 package org.example;
+import org.example.service.ShoppingCartService;
+import org.example.ui.TerminalUtils;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //create the service
+        ShoppingCartService scs = new ShoppingCartService();
+        //add io
+        TerminalUtils io = new TerminalUtils();
+        //add while loop -> make easier to exit loop, also easier to add condition if cart is empty
+        boolean running = true;
+
+        //why am i adding the variable running why not just say while true
+        while(running) {
+            io.displayMenu();
+            int choice = io.getIntInput("Enter choice: ");
+
+            /*
+            REMINDER
+            System.out.println("1. Display Cart");
+            System.out.println("2. Remove an Item");
+            System.out.println("3. Add an Item");
+            System.out.println("4. Checkout");
+             System.out.println("5. Exit");
+             */
+
+            switch (choice) {
+                case 1:
+                    if (scs.getItems().isEmpty()){
+                        io.displayMessage("Your cart is empty");
+
+                    } else {
+                        io.displayMessage("Your cart contains the following items: ");
+                        for (int i = 0; i < scs.getItems().size; i++) {
+                            Item item = scs.getItems().get(i);
+                            io.displayMessage(i)
+                        }
+                    }
+
+            }
+
+
+
+
+        }
+
+
     }
 }
