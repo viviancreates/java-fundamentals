5067c7551aa7dd5a8363df13664b1841d5488dfe
Vivian Davila <viviandavilacodes@gmail.com>
2025-06-25 14:52:33 -0700

    Continue refactor to add hash maps

--- assessments/shopping_cart_wip/src/main/java/org/example/service/ShoppingCartService.java
+++ assessments/shopping_cart_wip/src/main/java/org/example/service/ShoppingCartService.java
@@ -1,14 +1,18 @@
 package org.example.service;
 import org.example.model.Item;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class ShoppingCartService {
     private ArrayList<Item> items;
+    //this map tracks the key and value (String itemname, int quantity)
+    private HashMap<String, Integer> itemQuantity;
 
-    //creates new empty list
+    //creates new empty list and hash map
     public ShoppingCartService() {
         items = new ArrayList<>();
+        itemQuantity = new HashMap<>();
     }
 
     //REFACTOR TO USE ITEM FACTORY
@@ -19,6 +23,21 @@ public class ShoppingCartService {
     public void addItem(Item item) {
         //this is calling.add on the items list -> add this Item object to the ArrayList(Item) called items that lives inside cart
         items.add(item);
+
+        //REFACTOR for hashmap
+        //1. update addItem() toi track quantity in the quantity map
+        //item is created in the factory, then use the name as a key in the map
+        String name = item.getName();
+        //if the item is already added to the mao, increase count by one
+        if (itemQuantity.containsKey(name)) {
+            int currentQuantity = itemQuantity.get(name);
+            itemQuantity.put(name, currentQuantity + 1);
+            //otherwise add item to map with quantity 1
+        } else {
+            itemQuantity.put(name, 1);
+        }
+
+
     }
 
 
