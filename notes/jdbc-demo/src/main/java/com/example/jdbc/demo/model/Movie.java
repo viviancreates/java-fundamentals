b8c5c1fbf531a893a62502b3fbb38258a3b2fe4f
Vivian Davila <viviandavilacodes@gmail.com>
2025-07-18 11:35:53 -0500

    Add jdbc demo to notes

---
+++ notes/jdbc-demo/src/main/java/com/example/jdbc/demo/model/Movie.java
@@ -0,0 +1,52 @@
+package com.example.jdbc.demo.model;
+
+import java.time.LocalDate;
+
+// in some projects, you will see entity and model
+public class Movie {
+    private int movieID;
+    private String title;
+    private LocalDate releaseDate;
+    private int genreID;
+    private int ratingID;
+
+    public int getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieID(int movieID) {
+        this.movieID = movieID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getGenreID() {
+        return genreID;
+    }
+
+    public void setGenreID(int genreID) {
+        this.genreID = genreID;
+    }
+
+    public int getRatingID() {
+        return ratingID;
+    }
+
+    public void setRatingID(int ratingID) {
+        this.ratingID = ratingID;
+    }
+}
