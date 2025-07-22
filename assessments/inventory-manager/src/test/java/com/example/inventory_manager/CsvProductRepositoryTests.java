79c815b7c841a3b12fbdf30b2d6ae0bcb8953410
Vivian Davila <viviandavilacodes@gmail.com>
2025-07-22 16:50:40 -0500

    Pause, buggy tests

--- assessments/inventory-manager/src/test/java/com/example/inventory_manager/CsvProductRepositoryTests.java
+++ assessments/inventory-manager/src/test/java/com/example/inventory_manager/CsvProductRepositoryTests.java
@@ -1,34 +1,56 @@
 package com.example.inventory_manager;
 
+import com.example.inventory_manager.service.ProductService;
 import com.example.inventory_manager.model.*;
 import com.example.inventory_manager.repository.CsvProductRepository;
 import com.example.inventory_manager.repository.ProductRepository;
 import org.junit.jupiter.api.*;
 import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CsvProductRepositoryTests {
-    private static final String csvFilePath = "test.csv";
     private ProductRepository productRepository;
+    private ProductService productService;
 
     @BeforeEach
     void setUp() {
-        productRepository = new CsvProductRepository(csvFilePath);
+        File file = new File("data/test.csv");
+        if (file.exists()) {
+            file.delete();
+        }
+
+        //create a new empty file
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Could not reset test CSV file", e);
+        }
+
+        CSVProductRepository productRepository= new CSVProductRepository();
+        ps = new ProductService(productRepository);
     }
 
     @AfterEach
     void tearDown() {
         // Clean up test file after each test
-        File file = new File(csvFilePath);
+        File file = new File("data/test.csv");
         if (file.exists()) {
             file.delete();
         }
     }
 
 
+
+    @Test
+    @DisplayName("ProductService Initializes as Empty")
+    public void productServiceInitializedEmpty() {
+        List<Product> products = productService.getAllProducts();
+        assertTrue(products.isEmpty());
+    }
     /*
     @Test
     @DisplayName("Add a product from CSV")
