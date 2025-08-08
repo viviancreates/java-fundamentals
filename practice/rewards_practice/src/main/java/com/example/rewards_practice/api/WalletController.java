ad54a825a5378c5c25e078190cf5fc07a2fa4237
Vivian Davila <viviandavilacodes@gmail.com>
2025-08-07 23:38:19 -0700

    Pause

--- practice/rewards_practice/src/main/java/com/example/rewards_practice/api/WalletController.java
+++ practice/rewards_practice/src/main/java/com/example/rewards_practice/api/WalletController.java
@@ -10,24 +10,24 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class WalletController {
-
     private final CdpClient cdp;
+
     public WalletController(CdpClient cdp) { this.cdp = cdp; }
 
     @PostMapping("/wallets")
-    public Map<String, Object> createWallet() {
-        return cdp.createEvmAccount(); // returns { id, address, ... }
+    public Map<String, Object> createWallet() throws Exception {
+        return cdp.createEvmAccount();
     }
 
     public record FaucetReq(String address) {}
     @PostMapping("/faucet")
-    public Map<String, Object> faucet(@RequestBody FaucetReq req) {
+    public Map<String, Object> faucet(@RequestBody FaucetReq req) throws Exception {
         return cdp.requestFaucet(req.address());
     }
 
     public record SendReq(String from, String to, String amountEth) {}
     @PostMapping("/tx/send")
-    public Map<String, Object> send(@RequestBody SendReq req) {
+    public Map<String, Object> send(@RequestBody SendReq req) throws Exception {
         BigInteger wei = new BigDecimal(req.amountEth())
                 .multiply(new BigDecimal("1000000000000000000")) // 1e18
                 .toBigInteger();
